9a083d28c19ef184abd85492c906e37a19251d05
Rob Prouse <rob@prouse.org>
2022-12-15 22:50:44 -0500

    Include tiles and tilemap anc copy to VRAM

--- Template/build.bat
+++ Template/build.bat
@@ -1,7 +1,19 @@
 @echo off
+pushd "%~dp0"
 
 mkdir bin
+
+for %%i in (.) do set rom_name=%%~ni
+
 rgbasm -Werror -Weverything -Hl -o bin\main.o main.asm
+if %errorlevel% neq 0 goto end
 rgbasm -Werror -Weverything -Hl -o bin\sample.o sample.asm
-rgblink --dmg --tiny -map bin\sample.map --sym bin\sample.sym -o bin\sample.gb bin\main.o bin\sample.o
-rgbfix --title sample --pad-value 0 --validate --non-japanese bin\sample.gb
+if %errorlevel% neq 0 goto end
+rgblink --dmg --tiny --map bin\%rom_name%.map --sym bin\%rom_name%.sym -o bin\%rom_name%.gb bin\main.o bin\sample.o
+if %errorlevel% neq 0 goto end
+rgbfix --title sample --pad-value 0 --validate --non-japanese bin\%rom_name%.gb
+if %errorlevel% neq 0 goto end
+
+:end
+popd
+exit /b %errorlevel%
--- Template/sample.asm
+++ Template/sample.asm
@@ -1,11 +1,43 @@
 include "utils.inc"
 
+def TILES_COUNT					        equ (384)
+def BYTES_PER_TILE				      equ (16)
+def TILES_BYTE_SIZE				      equ (TILES_COUNT * BYTES_PER_TILE)
+def TILEMAP_BYTE_SIZE			      equ (1024)
+def GRAPHICS_DATA_SIZE			    equ (TILES_BYTE_SIZE + TILEMAP_BYTE_SIZE)
+def GRAPHICS_DATA_ADDRESS_END	  equ ($8000)
+def GRAPHICS_DATA_ADDRESS_START	equ (GRAPHICS_DATA_ADDRESS_END - GRAPHICS_DATA_SIZE)
+
+; load the graphics data from ROM to VRAM
+macro LoadGraphicsDataIntoVRAM
+  ld de, GRAPHICS_DATA_ADDRESS_START
+  ld hl, _VRAM8000
+.load_tile\@
+  ld a, [de]
+  inc de
+  ld [hli], a
+  ld a, d
+  cp a, high(GRAPHICS_DATA_ADDRESS_END)
+  jr nz, .load_tile\@
+endm
+
+;===============================================================================
 section "sample", rom0
 
 InitSample:
+  LoadGraphicsDataIntoVRAM
+
+  ; Turn the LCD on
+  ld a, LCDCF_ON
+  ld [rLCDC], a
   ret
 
 UpdateSample:
   ret
 
 export InitSample, UpdateSample
+
+;===============================================================================
+section "graphic_data", rom0[GRAPHICS_DATA_ADDRESS_START]
+incbin "tileset.chr"
+incbin "background.tlm"

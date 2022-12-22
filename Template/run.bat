b93576fd4cf1b090f64a3110e044c91235db03d1
Rob Prouse <rob@prouse.org>
2022-12-22 08:16:51 +0000

    Add a window to the bottom of the screen

---
+++ Template/run.bat
@@ -0,0 +1,3 @@
+@echo off
+
+bgb64 --rom .\bin\Template.gb
--- Template/sample.asm
+++ Template/sample.asm
@@ -3,8 +3,10 @@ include "utils.inc"
 def TILES_COUNT					        equ (384)
 def BYTES_PER_TILE				      equ (16)
 def TILES_BYTE_SIZE				      equ (TILES_COUNT * BYTES_PER_TILE)
-def TILEMAP_BYTE_SIZE			      equ (1024)
-def GRAPHICS_DATA_SIZE			    equ (TILES_BYTE_SIZE + TILEMAP_BYTE_SIZE)
+def TILEMAPS_COUNT              equ (2)
+def BYTES_PER_TILEMAP           equ (1024)
+def TILEMAPS_BYTE_SIZE			    equ (TILEMAPS_COUNT*BYTES_PER_TILEMAP)
+def GRAPHICS_DATA_SIZE			    equ (TILES_BYTE_SIZE + TILEMAPS_BYTE_SIZE)
 def GRAPHICS_DATA_ADDRESS_END	  equ ($8000)
 def GRAPHICS_DATA_ADDRESS_START	equ (GRAPHICS_DATA_ADDRESS_END - GRAPHICS_DATA_SIZE)
 
@@ -31,8 +33,20 @@ InitSample:
 
   LoadGraphicsDataIntoVRAM
 
+  ; Place the Background on the screen
+  ld a, 96
+  ld [rSCX], a
+  ld a, 64
+  ld [rSCY], a
+
+  ; Place the Window on the screen
+  ld a, 7
+  ld [rWX], a
+  ld a, 120
+  ld [rWY], a
+
   ; Turn the LCD and background on
-  ld a, LCDCF_ON | LCDCF_BG8800 | LCDCF_BG9800 | LCDCF_BGON
+  ld a, LCDCF_ON | LCDCF_WIN9C00 | LCDCF_WINON | LCDCF_BG8800 | LCDCF_BG9800 | LCDCF_BGON
   ld [rLCDC], a
 
   ; Enable the vblank interupt
@@ -45,12 +59,13 @@ UpdateSample:
   ; Wait for the vBlank interupt
   halt
 
-  ld a, [rSCX]
-  inc a
-  ld [rSCX], a
-  ld a, [rSCY]
-  inc a
-  ld [rSCY], a
+  ; Scroll the background
+  ; ld a, [rSCX]
+  ; inc a
+  ; ld [rSCX], a
+  ; ld a, [rSCY]
+  ; inc a
+  ; ld [rSCY], a
   ret
 
 export InitSample, UpdateSample
@@ -63,3 +78,4 @@ section "vblank_interupt", rom0[$0040]
 section "graphic_data", rom0[GRAPHICS_DATA_ADDRESS_START]
 incbin "tileset.chr"
 incbin "background.tlm"
+incbin "window.tlm"
